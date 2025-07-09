628d37396308d35386ef0da8151c40d41c04f69f
Sul6an <35851517+BLANKxSS@users.noreply.github.com>
2025-07-09 17:49:01 +0300

    feat: integrate Tailwind CSS and daisyUI, add routing and new pages
    - Updated package.json to include Tailwind CSS, daisyUI, autoprefixer, and postcss.
    - Configured postcss to use Tailwind CSS and autoprefixer.
    - Implemented a Navbar component and set up routing for SignUp, Login, Settings, and Profile pages.
    - Created placeholder components for HomePage, LoginPage, ProfilePage, SettingsPage, and SignUpPage.
    - Enhanced tailwind.config.js to include daisyUI themes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,23 @@
-export const App = () => {
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";  
+
+const App = () => {
   return (
-    <div className="text-red-400">gg</div>
-  )
+    <div>
+
+      <Navbar />
+
+      <Routes>
+      <Route path="/signup" element={<SignUpPage /> } />
+      <Route path="/login" element={ <LoginPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
-export default App
+
+export default App;
